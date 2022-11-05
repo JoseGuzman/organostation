e6ec5e19a21af58e79d21a6e3d6062d1ce749cf2
JoseGuzman <sjm.guzman@gmail.com>
2022-11-05 20:39:01 +0100

    add callback

--- dashboards/testboard/__init__.py
+++ dashboards/testboard/__init__.py
@@ -10,6 +10,7 @@ https://www.youtube.com/watch?v=7m0Bq1EGPPg
 from flask import Flask
 
 from dash import Dash, html, dcc
+from dash import Output, Input
 import dash_bootstrap_components as dbc
 
 import plotly.express as px
@@ -25,13 +26,29 @@ def test_layout(flask_app:Flask) -> Dash:
     mydashboard = Dash(server = flask_app,
         name = 'TestBoard',
         url_base_pathname = '/testboard/',
-        external_stylesheets=[dbc.themes.BOOTSTRAP]
+        external_stylesheets=[dbc.themes.SOLAR]
         )
 
-    # define components 
+    # define components  
     mytext = dcc.Markdown(children='# Hello World in TestBoard')
+    myinput = dbc.Input(value='Insert your message here')
+
+    # callback decorator and callback function
+    @mydashboard.callback(
+        Output(mytext, component_property='children'),
+        Input(myinput, component_property='value')
+    )
+
+    def update_title(mytext):
+        """
+        Function argument comes from component_property of input
+        """
+        # returns componenent_property of the output:w
+        return mytext
+
+    # finish layout
     mydashboard.title = "testboard"
-    mydashboard.layout = dbc.Container([mytext])
+    mydashboard.layout = dbc.Container([mytext, myinput])
 
     return mydashboard
 
