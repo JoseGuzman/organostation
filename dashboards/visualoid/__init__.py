895d1f470b91ee005a9a1d175fe9f1a8862f809a
JoseGuzman <sjm.guzman@gmail.com>
2022-11-21 15:06:39 +0100

    clean visualoid init app

--- dashboards/visualoid/__init__.py
+++ dashboards/visualoid/__init__.py
@@ -8,9 +8,10 @@ This dashboard runs under flask
 Visit https://github.com/okomarov/dash_on_flask for details
 Check this video: https://www.youtube.com/watch?v=XOFrvzWFM7Y
 
-The Flask application will call the dashboards defined here.
-(a dash object). The dash object is the argument of a layout.
-Layouts contain pre-designed components and callbacks and are 
+This file creates the dass application.
+The Flask application will call the dashboards defined here
+(i.e, the Dash object). The Dash object is the argument of a layout.
+Layouts contain pre-designed components and callbacks and are
 defined in .scr.components
 """
 from flask import Flask
@@ -18,7 +19,7 @@ from flask import Flask
 from dash import Dash
 import dash_bootstrap_components as dbc # for themes
 
-# layouts take dash as argument
+# layouts take Dash object as an  argument
 from .src.components import layout
 
 def test_dashboard(flask_app:Flask) -> Dash:
@@ -26,16 +27,16 @@ def test_dashboard(flask_app:Flask) -> Dash:
     Creates a test dashboard inside a Flask application
     """
     # main Dash applicaton
-    mydashboard = Dash( 
-        server = flask_app, 
-        name = 'Visualoid', 
+    mydashboard = Dash(
+        server = flask_app,
+        name = 'Visualoid',
         url_base_pathname = '/visualoid/',
         external_stylesheets = [dbc.themes.SOLAR]
     )
     mydashboard.title = "Visualoid"
     # dash layout in src/components/layout
-    #mydashboard.layout = layout.simple_layout(dashboard = mydashboard) 
-    mydashboard.layout = layout.error_404(dashboard = mydashboard) 
+    #mydashboard.layout = layout.simple_layout(dashboard = mydashboard)
+    mydashboard.layout = layout.error_404(dashboard = mydashboard)
     return mydashboard
 
 def test_callback(flask_app:Flask) -> Dash:
