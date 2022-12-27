f7399864fa5af64aa7f40d7e050bc47fa8157852
JoseGuzman <sjm.guzman@gmail.com>
2022-12-27 12:15:48 +0100

    update home blueprint

---
+++ organostation/home/views.py
@@ -0,0 +1,46 @@
+"""
+views.py
+Created: Sun Dec 25 21:10:17 CET 2022
+
+It creates the routes (or views) for our 'home'. Home uses a 
+'home_bp' blueprint that define access to basic resources
+from the home webpage. 
+We define routes, templates and logic of the homepage here. 
+"""
+from flask import Blueprint, render_template
+
+home_bp = Blueprint(
+    "home_bp", __name__, template_folder="templates", static_folder="static"
+)
+
+
+@home_bp.route("/")
+def home():
+    """Homepage visible to all users"""
+    return render_template(
+        "home.jinja2",  # global template
+        title="Organostation App",
+        description="Electrophysiology in a box",
+    )
+
+
+@home_bp.route("/test")
+def test():
+    """Test page"""
+    return render_template("test.jinja2", title="Test Page", description="simple test")
+
+
+@home_bp.route("/specs")
+def specs():
+    """Technical specifications"""
+    return render_template(
+        "specs.jinja2", title="Specifications", description="technical specifications"
+    )
+
+
+@home_bp.route("/register")
+def register():
+    """Register page"""
+    return render_template(
+        "register.jinja2", title="Register", description="Register page"
+    )
