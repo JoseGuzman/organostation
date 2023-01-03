c9501a08095d7ee445faf50805f0db7f55e4ac10
JoseGuzman <sjm.guzman@gmail.com>
2023-01-03 18:58:13 +0100

    update images for forms

---
+++ organostation/home/forms.py
@@ -0,0 +1,34 @@
+"""Form object declaration."""
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class ContactForm(FlaskForm):
+    """Contact us form with name, email and body."""
+
+    name = StringField("Name", validators=[DataRequired()])
+
+    email = StringField(
+        "Email",
+        validators=[Email(message=("Not a valid email address.")), DataRequired()],
+    )
+
+    body = TextAreaField(
+        "Message",
+        validators=[
+            DataRequired(),
+            Length(min=4, message=("Your message is too short.")),
+        ],
+    )
+    recaptcha = RecaptchaField()
+    submit = SubmitField("Submit")
+
+
+class LoginForm(FlaskForm):
+    """Login form with username and password."""
+
+    email = StringField("Email", validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField(" Remember Me")
+    submit = SubmitField("Log In")
