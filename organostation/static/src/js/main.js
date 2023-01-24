92d80cdc77b60a6f018f2474f317f59a86e8fba8
JoseGuzman <sjm.guzman@gmail.com>
2023-01-24 16:37:38 +0100

    update menubar

--- organostation/static/src/js/main.js
+++ organostation/static/src/js/main.js
@@ -8,13 +8,13 @@ $('.mybtn').click(function () {
 });
 
 
-$('.menusidebar ul li a').click(function () {
+$('.nav-link ul li a').click(function () {
   var id = $(this).attr('id');
-  $('.menusidebar ul li ul.item-show-' + id).toggleClass("show");
-  $('.menusidebar ul li #' + id + ' span').toggleClass("rotate");
+  $('.nav-link ul li ul.item-show-' + id).toggleClass("show");
+  $('.nav-link ul li #' + id + ' span').toggleClass("rotate");
 
 });
 
-$('.menusidebar ul li').click(function () {
+$('.nav-link ul li').click(function () {
   $(this).addClass("active").siblings().removeClass("active");
 });
